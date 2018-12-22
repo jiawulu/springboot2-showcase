4ff2825d4b0b1c3d54414511fee01b3edb4cbf0d
wuzhong <wuzhong@taobao.com>
2018-12-22 21:20:56 +0800

    :sparkles: add result model

---
+++ webapp/src/main/java/com/wuzhong/commons/result/Result.java
@@ -0,0 +1,45 @@
+package com.wuzhong.commons.result;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Result<T> implements Serializable {
+
+    private T result;
+
+    private Integer code;
+
+    private String message;
+
+    private boolean success = true;
+
+    private Result() {
+    }
+
+    /**
+     * @param data
+     * @param <T>
+     * @return
+     */
+    public static <T> Result<T> ok(T data) {
+        Result<T> result = new Result<>();
+        result.setResult(data);
+
+        return result;
+    }
+
+    /**
+     * @return
+     */
+    public static <T> Result<T> err(Integer code, String message) {
+        Result<T> result = new Result<>();
+        result.code = code;
+        result.message = message;
+        result.success = false;
+        return result;
+    }
+
+
+}
