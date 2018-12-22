dcdc99dba7afdd1ab8720fd17866f7d268a13283
wuzhong <wuzhong@taobao.com>
2018-12-22 13:07:24 +0800

    ✨ hot deploy

--- webapp/src/main/java/com/wuzhong/webapp/StarterPrinter.java
+++ webapp/src/main/java/com/wuzhong/webapp/StarterPrinter.java
@@ -1,14 +1,9 @@
 package com.wuzhong.webapp;
 
 import com.wuzhong.starter.StarterService;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -24,9 +19,14 @@ public class StarterPrinter implements InitializingBean {
     public void afterPropertiesSet() throws Exception {
         log.warn("StarterPrinter afterPropertiesSet  >>>> ");
 
-        for (StarterService starterService : starterServices){
+        for (StarterService starterService : starterServices) {
             starterService.start();
         }
+
+//      hot deploy
+//      add devtools dependency
+//      edit ->  build -> recompile java
+        System.out.println(">>>>");
     }
 
 }
