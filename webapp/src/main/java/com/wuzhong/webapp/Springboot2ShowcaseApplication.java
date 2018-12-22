17671eb3e555e609e69cbdfea67db506cd55100a
wuzhong <wuzhong@taobao.com>
2018-12-22 15:31:10 +0800

    ✨ add swagger http://localhost:8000/swagger-ui.html

--- webapp/src/main/java/com/wuzhong/webapp/Springboot2ShowcaseApplication.java
+++ webapp/src/main/java/com/wuzhong/webapp/Springboot2ShowcaseApplication.java
@@ -1,9 +1,11 @@
 package com.wuzhong.webapp;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableSwagger2Doc
 @EnableConfigurationProperties
 @SpringBootApplication
 public class Springboot2ShowcaseApplication {
